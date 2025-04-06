35c6932fc718eff32e2755830a4868a10211c8f2
mahmud-alam <official.mahmud23@gmail.com>
2025-04-06 15:08:17 +0600

    model trained and saved as model.pkl

--- backend/model.py
+++ backend/model.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+import pickle
 
 data = {
     'area': [1000, 1500, 2000, 2500, 3000],
@@ -6,3 +8,12 @@ data = {
 }
 
 df = pd.DataFrame(data)
+
+model = LinearRegression()
+model.fit(df[['area']], df['price'])
+
+# Save the trained model
+with open('model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+print("Model trained and saved as model.pkl")
