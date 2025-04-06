8ea3c64b8187918f8e10a322ebe20283007ea7d5
mahmud-alam <official.mahmud23@gmail.com>
2025-04-06 16:23:20 +0600

    change prediction model: LinearRegression to polynomial regression model

--- backend/model.py
+++ backend/model.py
@@ -1,15 +1,20 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 import pickle
 
 data = {
     'area': [1000, 1500, 2000, 2500, 3000],
-    'price': [100000, 150000, 200000, 250000, 300000]
+    'price': [500000, 700000, 950000, 1350000, 2000000]
 }
 
 df = pd.DataFrame(data)
 
-model = LinearRegression()
+# model = LinearRegression()
+
+# Use polynomial regression (degree 2)
+model = make_pipeline(PolynomialFeatures(degree=2), LinearRegression())
 model.fit(df[['area']], df['price'])
 
 # Save the trained model
